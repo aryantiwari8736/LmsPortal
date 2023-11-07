da37c8fd2de899632b91c814159cb415078205dc
aryan8736 <aryan.2125cs1071@kiet.edu>
2023-11-07 23:44:49 +0530

    new mailer function added

--- server/utills/sendMail.ts
+++ server/utills/sendMail.ts
@@ -42,5 +42,38 @@ const sendMail = async (options:EmailOptions):Promise <void>=>{
    }
     } 
     
+// update -- 
+  const  newMail = async (options:EmailOptions):Promise <void>=>{
+        try {
+         
+         const transporter:Transporter = nodemailer.createTransport({
+             host:process.env.SMTP_HOST,
+             port:parseInt(process.env.SMTP_PORT),
+             service:process.env.SMTP_SERVICE,
+             auth:{
+                 user:process.env.SMTP_MAIL,
+                 pass:process.env.SMTP_PASSWORD
+     
+             }
+         })
+     
+         const {email,subject,template,data} = options;
+         //get the path to email template file - 
+         const templatePath = path.join(__dirname,"../mails",template)
+     
+         //render the email template with ejs - 
+         const html:string = await ejs.renderFile(templatePath,data);
+     
+         const mailOptions = {
+             from:process.env.SMTP_MAIL,
+             to:email,
+             subject,
+             html
+         }
+         await transporter.sendMail(mailOptions);
+        } catch (error) {
+         
+        }
+         } 
 
 export default sendMail;
